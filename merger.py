cf4745d40bed9ba331a708250e1f1577d78b8748
Harrison Sheldon <your_email_address@example.com>
2023-12-18 09:53:52 -0800

    update python library to use more current PyPDF2 version

--- merger.py
+++ merger.py
@@ -1,11 +1,12 @@
 import os
-import PyPDF2
-import sys
+from PyPDF2 import PdfWriter
 
-merger = PyPDF2.PdfFileMerger()
+merger = PdfWriter()
 
 for filename in os.listdir(os.curdir):
     if filename.endswith(".pdf"):
         print(filename)
         merger.append(filename)
     merger.write("combinedDocs.pdf")
+
+merger.close()
