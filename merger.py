cfe570dbc36e3c2331d633384ccb3835fb182d1c
Harrison Sheldon <your_email_address@example.com>
2023-12-18 16:09:48 -0800

    add ability to combine documents from a directory

--- merger.py
+++ merger.py
@@ -2,11 +2,19 @@ import os
 from PyPDF2 import PdfWriter
 
 merger = PdfWriter()
+path = '/Users/hrsheldon/PycharmProjects/PDFMerger/docsToMerge'  # absolute path to directory with files
+files = os.listdir(path)
 
-for filename in os.listdir(os.curdir):
+# Loop through files in the specified directory
+for filename in files:
     if filename.endswith(".pdf"):
-        print(filename)
-        merger.append(filename)
-    merger.write("combinedDocs.pdf")
+        print(f"Appending: {filename}")
+        file_path = os.path.join(path, filename)  # Get full file path
+        merger.append(file_path)  # append file to end
 
+# Write the combined PDF file after appending all PDFs
+with open("combinedDocs.pdf", "wb") as output_pdf:
+    merger.write(output_pdf)
+
+# Close the PdfWriter object after writing the file
 merger.close()
